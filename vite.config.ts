72b4389ea65542f4ad153ddb98f45fe1c341dbed
Saber Alipour <alipouor@gmail.com>
2024-03-03 14:30:17 +0330

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,8 @@
-import path, { resolve } from 'node:path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import * as packageJson from './package.json'
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-      include: ['src/utils/'],
-    }),
-    react(),
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
-  ],
-  build: {
-    lib: {
-      entry: path.join('src', 'utils/index.ts'),
-
-      name: 'ReactViteLibrary',
-      formats: ['es', 'umd'],
-      fileName: (format) => `react-vite-library.${format}.js`,
-    },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-}))
+export default defineConfig({
+  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
+  resolve: { alias: { src: resolve('src/') } },
+});
